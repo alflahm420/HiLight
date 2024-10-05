ec571a404538e6e1001416cdebada41e4890f540
alflahm420 <74799275+alflahm420@users.noreply.github.com>
2024-10-05 20:14:29 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('submitBtn').addEventListener('click', function () {
+    const number = document.getElementById('numberInput').value;
+    alert("Number entered: " + number);
+});
